a45e3b1d5358261c23d91aa21a97f5139d587a42
Péter Kovács <peter.kovacs0519@gmail.com>
2016-05-03 19:27:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Spring WebMVC configuration change from XML to Java/annotation.
+
+For full tutorial please visit my blog at http://psprog.hu (only available in hungarian right now).
+
+Clone this repository, then change to config-xml or config-java branch to see configurations separated.
---
+++ src/main/java/hu/psprog/dev/tutorial/web/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hu.psprog.dev.tutorial.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/home")
+    public ModelAndView home() {
+        
+        return new ModelAndView("home");
+    }
+}
